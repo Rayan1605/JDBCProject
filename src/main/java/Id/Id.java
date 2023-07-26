b0ac8ee4207e90396fcc50022de8a6799e232405
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-26 20:42:14 +0100

    TestingTheConnection

--- src/main/java/Id/Id.java
+++ src/main/java/Id/Id.java
@@ -3,6 +3,9 @@ package Id;
 import DbConnection.DataBaseConnection;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +16,20 @@ public class Id {
     private final List<Integer> id = new ArrayList<>();
     Connection con = DataBaseConnection.CreateConnectionTouserdetails();
     Connection connection = DataBaseConnection.createConnectionToTeethTreatment();
+
+    private void  GettingTheId()  {
+        String query="SELECT * FROM theid";
+        try {
+            Statement statement2 = con.createStatement();
+            ResultSet result = statement2.executeQuery(query);
+            while (result.next()) {
+                this.id.add(result.getInt("Id"));
+            }
+        }  catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 
 }
