772037fa773df0a24b20dd4876c2134a9d0aecc7
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-26 21:05:17 +0100

    TestingTheConnection

--- src/test/java/TestingPatient/PatientTesting.java
+++ src/test/java/TestingPatient/PatientTesting.java
@@ -1,8 +1,24 @@
 package TestingPatient;
 
 import Patient.Patient;
+import org.junit.jupiter.api.BeforeEach;
 
 public class PatientTesting {
 Patient patient = new Patient();
 
+     @BeforeEach
+     public void Set() {
+        patient.setID(1);
+        patient.setFirstname("John");
+        patient.setEmail("John@gmail.com");
+        patient.setLastName("Smith");
+        patient.setPhoneNumber(123456789);
+        patient.setAddress("PoliceStation");
+
+        patient.setDateOfBirthday("01/01/2000");
+        patient.setDateOfTreatment("01/01/2020");
+        patient.setAddress("LeasNaCoille");
+        patient.setSpecialNeeds(true);
+        patient.setTypeOfTreatment("Filling");
+    }
 }
