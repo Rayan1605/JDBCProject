c669fdda16ad47446b31b476af22adb6587b57a8
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-26 20:41:59 +0100

    TestingTheConnection

--- src/main/java/Id/Id.java
+++ src/main/java/Id/Id.java
@@ -1,4 +1,18 @@
 package Id;
 
+import DbConnection.DataBaseConnection;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Id {
+    //This is the class that will all id in the database
+    // it will check if the id is already in the database
+    // and delete the id if it is already there
+    private final List<Integer> id = new ArrayList<>();
+    Connection con = DataBaseConnection.CreateConnectionTouserdetails();
+    Connection connection = DataBaseConnection.createConnectionToTeethTreatment();
+
+
 }
