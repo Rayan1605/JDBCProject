d97fb9cdf4a30bcd085e49abb3fb0ae3ff91dda9
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-26 20:31:29 +0100

    Start

--- src/main/java/DbConnection/DataBaseConnection.java
+++ src/main/java/DbConnection/DataBaseConnection.java
@@ -1,4 +1,11 @@
 package DbConnection;
 
+import java.sql.Connection;
+
 public class DataBaseConnection {
+    static Connection connection;
+
+    public static Connection createConnectionToTeethTreatment() {
+        return connection;
+    }
 }
