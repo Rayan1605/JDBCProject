42a9551db09e6be674ceb6a26a2cce40877209aa
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-26 21:11:56 +0100

    TestingTheConnection

--- src/main/java/DataBaseImplement/DatabaseInterface.java
+++ src/main/java/DataBaseImplement/DatabaseInterface.java
@@ -1,5 +1,18 @@
 package DataBaseImplement;
 
+import Patient.Patient;
+
 public interface DatabaseInterface {
+    boolean createPatient(Patient patient, String DatabaseName); //Used to create
+    void showAllPatient(String DatabaseName); // show all Patient
+    void showPatientBasedonID(int id, String DatabaseName); // show patient on id
+    PatientClass ImportPatient(int id);//Import Patient from database
+    //UPDATE
+    boolean updatePatient (int id, String itemtoUpdate, String newValue, int index , String DatabaseName);
+    //DELETE
+    void deletePatient (int id,String DatabaseName,String[] TableNames);
+    //GettingPassword
+    String GetPassword (String databaseName);
+    void SetPassword (String password, String databaseName);
 
 }
