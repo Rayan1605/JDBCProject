61eae56c21d727bfd23b5f2a40faf7220a36d17e
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-26 21:01:25 +0100

    TestingTheConnection

--- src/main/java/Patient/Patient.java
+++ src/main/java/Patient/Patient.java
@@ -1,4 +1,16 @@
 package Patient;
 
 public class Patient {
+    private int ID;
+    private String Firstname;
+    private String dateOfBirthday;
+    private String dateOfTreatment;
+
+    private String address;
+    private boolean needspecialNeeds;
+
+    private String typeOfTreatment;
+    private int phoneNumber;
+    private String email;
+    private String LastName;
 }
