e41072ac5cc1aaadbb90f78fab3fba7f90a13b92
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-26 20:31:41 +0100

    Start

--- src/main/java/DbConnection/DataBaseConnection.java
+++ src/main/java/DbConnection/DataBaseConnection.java
@@ -1,11 +1,26 @@
 package DbConnection;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DataBaseConnection {
     static Connection connection;
 
     public static Connection createConnectionToTeethTreatment() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); // getting the driver
+            // creating the connection with the database
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/teethtreatment?useSSL=false"
+                    ,"root"
+                    ,"Password");
+
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return connection;
     }
 }
