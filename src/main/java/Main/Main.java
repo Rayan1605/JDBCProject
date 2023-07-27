d1aa1980d4eb65faf2b7ae98029bbeb5771f4c4d
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-27 20:30:57 +0100

    TestingTheConnection

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,7 +1,33 @@
 package Main;
 
+import DataBaseImplement.DatabaseCrudOperation;
+import DataBaseImplement.DatabaseInterface;
+import Id.Id;
+
+import java.util.Scanner;
+
 public class Main {
+    static Scanner myInput = new Scanner(System.in);
+    static String[] tables = {"orthodontistclinic", "dentaldepartment"};
+    static DatabaseInterface implement = new DatabaseCrudOperation();
+    static String DatabaseName;
+    static int count = 0;
+    static Id id = new Id();
+
     public static void main(String[] args) {
+        boolean exitApplicaton = false;
+        System.out.println("Welcome to Dental Clinic!\n");
+        databasetoEnter();//Which department to enter the database
 
     }
+
+
+
+
+
+
+
+
+
+
 }
